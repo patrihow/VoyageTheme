6d955eab48f18d931836810b03ef172260663cca
Patricia <pbravo.art@gmail.com>
2025-03-28 18:17:10 -0400

    Ajout du support de la balise title dans les fonctionnalités du thème

---
+++ footer.php
@@ -0,0 +1,70 @@
+
+
+<footer class="site-footer">
+      <div class="site-footer__inner container container--narrow">
+        <div class="group">
+          <div class="site-footer__col-one">
+            <h1 class="school-logo-text school-logo-text--alt-color">
+              <a href="#">Club de <strong>Voyage</strong></a>
+            </h1>
+            <p><a class="site-footer__link" href="#">+51 222 5555</a></p>
+            
+          </div>
+
+          <div class="site-footer__col-two-three-group">
+            <div class="site-footer__col-two">
+              <h3 class="headline headline--small">Explore</h3>
+              <nav class="nav-list">
+                <ul>
+                  <li><a href="#">Item</a></li>
+                  <li><a href="#">Item</a></li>
+                  <li><a href="#">Item</a></li>
+                  <li><a href="#">Item</a></li>
+                  <li><a href="#">Item</a></li>
+                  <li><a href="#">Item</a></li>
+                </ul>
+              </nav>
+            </div>
+
+            <div class="site-footer__col-three">
+              <h3 class="headline headline--small">Contacte-nous</h3>
+              <nav class="nav-list">
+                <ul>
+                  <li><a href="#">Patricia Bravo</a></li>
+                  <li><a href="#">contact@voyage.com</a></li>
+                  <li><a href="#">Montréal, QC, Canada</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="site-footer__col-four">
+            <h3 class="headline headline--small">Réseaux sociaux</h3>
+            <nav>
+              <ul class="min-list social-icons-list group">
+                <li>
+                  <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
